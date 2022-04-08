3609f215b76088894d7dc74dbbf1df6e44c0bfff
noor3030 <nnorkrka@gmail.com>
2022-04-08 23:51:39 +0300

    make 4 input bytes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home pa-10">
+    <v-row justify="center">
+      <v-flex xs4 md2>
+        <v-text-field label="Byte1" outlined v-model="byte1"></v-text-field
+      ></v-flex>
+      <v-flex xs4 md2>
+        <v-text-field label="Byte2" outlined v-model="byte2"></v-text-field
+      ></v-flex>
+      <v-flex xs4 md2>
+        <v-text-field label="Byte3" outlined v-model="byte3"></v-text-field
+      ></v-flex>
+      <v-flex xs4 md2>
+        <v-text-field label="Byte4" outlined v-model="byte4"></v-text-field
+      ></v-flex>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      byte1: null,
+      byte2: null,
+      byte3: null,
+      byte4: null,
+    };
   },
+  name: "Home",
+ computed:{
+   
+ }
 });
 </script>
